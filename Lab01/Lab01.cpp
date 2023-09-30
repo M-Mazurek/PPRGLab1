6c025f15633a2b2c330bc8cc9ffc1b24748d61bb
Michał Mazurek <s30902@pjwstk.edu.pl>
2023-09-30 17:06:07 +0200

    Added ex. 1 & 2

--- Lab01/Lab01.cpp
+++ Lab01/Lab01.cpp
@@ -2,8 +2,60 @@
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
-	std::cout << "xpp" << std::endl;
+	// ex. 1
+	/*
+	int a,b,c;
+
+	std::cout << "Podaj a: ";
+	std::cin >> a;
+
+	std::cout << "Podaj b: ";
+	std::cin >> b;
+
+	std::cout << "Podaj c: ";
+	std::cin >> c;
+
+	system("cls");
+
+	if (a < b + c && b < a + c && c < a + b)
+		printf("Boki %d, %d, %d tworza trojkat.", a, b, c);
+	else
+		printf("Boki %d, %d, %d nie tworza trojkatu.", a, b, c);
+	
+	std::cout << std::endl;
+	*/
+	// ex. 2
+	
+	srand((unsigned)time(NULL));
+	const int max = 1000;
+	int val = rand() % max + 1;
+	int chunkSize = max;
+	int guess = max / 2;
+	int attempts = 0;
+
+	std::cout << "Looking for " << val << std::endl << std::endl;
+	
+	while (true)
+	{
+		attempts++;
+		chunkSize = ceil(static_cast<float>(chunkSize) / 2.0);
+		std::cout << "Guessing " << guess << std::endl;
+		if (guess == val)
+		{
+			std::cout << "Correct, attempts: " << attempts << std::endl << std::endl;
+			break;
+		}
+		else if (guess > val)
+		{
+			std::cout << "Lower" << std::endl << std::endl;
+			guess -= chunkSize / 2;
+		}
+		else
+		{
+			std::cout << "Higher" << std::endl << std::endl;
+			guess += chunkSize / 2;
+		}
+	}
 
 	return 0;
 }
