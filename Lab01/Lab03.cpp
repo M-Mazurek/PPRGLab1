67678cef46ffd3179b4e1bbc514d5811588434fb
m.mazurekmail@gmail.com <m.mazurekmail@gmail.com>
2023-11-18 14:53:57 +0100

    Lab03.02

--- Lab01/Lab03.cpp
+++ Lab01/Lab03.cpp
@@ -5,9 +5,25 @@ bool zad1_is_even(int x)
     return x % 2 == 0;
 }
 
+int a(1), b(2);
+
+void zad2_swap_globals()
+{
+    int t = a;
+    a = b;
+    b = t;
+}
+
 int main()
 {
     // ex. 1
     //std::cout << zad1_is_even(2) << std::endl;
     //std::cout << zad1_is_even(1) << std::endl;
+
+    // ex. 2
+    printf("%d %d\n", a, b);
+    zad2_swap_globals();
+    printf("%d %d\n", a, b);
+
+    return 0;
 }
