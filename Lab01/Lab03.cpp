dfd592134c367b63ebf0a4631f6e7938e9c1a9e3
m.mazurekmail@gmail.com <m.mazurekmail@gmail.com>
2023-11-18 14:50:52 +0100

    Lab03.01

--- Lab01/Lab03.cpp
+++ Lab01/Lab03.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+bool zad1_is_even(int x)
+{
+    return x % 2 == 0;
+}
+
 int main()
 {
-
+    // ex. 1
+    //std::cout << zad1_is_even(2) << std::endl;
+    //std::cout << zad1_is_even(1) << std::endl;
 }
